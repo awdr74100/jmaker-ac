2f54e9f69a3c84c54920690a1412f8a02acc6231
藍奕濡 <a78945612385238@gmail.com>
2020-07-20 19:02:11 +0800

    add other config

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    port: 3001,
+    open: true,
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@img': '@/assets/img',
+        '@component': '@/components',
+      },
+    },
+  },
+};
